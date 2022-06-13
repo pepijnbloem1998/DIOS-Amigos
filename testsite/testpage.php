4e0103e71408ec6d25ac929969d7a17d366e3bb8
pepijnbloem1998 <63612000+pepijnbloem1998@users.noreply.github.com>
2022-06-13 16:35:41 +0200

    Update testpage.php

--- testsite/testpage.php
+++ testsite/testpage.php
@@ -8,7 +8,7 @@
     {
       if($_POST['txt_naam']!="" && $_POST['txt_password']!="")
       {
-        $url="http://10.43.166.250:5051/valideer/" . $_POST['txt_naam'] . "/" . $_POST['txt_password'];
+        $url="http://10.1.4.10:5051/valideer/" . $_POST['txt_naam'] . "/" . $_POST['txt_password'];
         $html1=file_get_contents($url);
       }
       else
@@ -18,7 +18,7 @@
     }
   if($focus==2)
    {
-     $url="http://10.43.199.202:5052/hoofdstad/" . $_POST['txt_land'];
+     $url="http://10.1.4.10:5052/hoofdstad/" . $_POST['txt_land'];
      $html2="hoofdstad van " . $_POST['txt_land'] . " is " . file_get_contents($url);
    }
 
